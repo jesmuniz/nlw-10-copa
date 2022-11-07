06209364b6a72e5124d0b1e595c297860e0c847d
Lucas de Sousa <Lucasads18@outlook.com>
2022-11-07 12:48:11 -0300

    correção para a Jessy

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ function createGame (player1, hour, player2) {
   <li>
                             <img src="./assets/icon-${player1}.svg" alt="Bandeira do ${player1}">
                             <strong>${hour}</strong>
-                            <img src="/assets/icon-${player2}.svg" alt="Bandeira de ${player2}">
+                            <img src="./assets/icon-${player2}.svg" alt="Bandeira de ${player2}">
                         </li>
   `
 }
